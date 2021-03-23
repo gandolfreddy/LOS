05356ae45de9db35d069e54c04fb0bd1f3494e4e
Gandolfreddy <freddy.wei0810@gmail.com>
2021-03-24 02:51:17 +0800

    first add
    upload related files.

---
+++ script.js
@@ -0,0 +1,34 @@
+generateRandomStep();
+generateRandomEvent();
+
+function showStep() {
+    // Generate a random number from 1 to 8
+    const RandomNum = Math.floor(Math.random() * 8) + 1;
+
+    // images/dice_1.png upto images/dice_8.png
+    const DiceImage = `assets/dice_${RandomNum}.png`;
+
+    document.querySelectorAll("img")[0].setAttribute("src", DiceImage);
+}
+
+function generateRandomStep() {
+    for (let i=1; i<=20; i++) {
+        setTimeout(showStep, 50*i);
+    }
+}
+
+function showEvent() {
+    // Generate a random number from 1 to 4
+    const RandomNum = Math.floor(Math.random() * 4) + 1;
+
+    // images/dice_1.png upto images/dice_6.png
+    const DiceImage = `assets/event_${RandomNum}.png`;
+
+    document.querySelectorAll("img")[1].setAttribute("src", DiceImage);
+}
+
+function generateRandomEvent() {
+    for (let i=1; i<=20; i++) {
+        setTimeout(showEvent, 50 * i);
+    }
+}
